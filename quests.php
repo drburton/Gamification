8ea9975ee6d51c24717588c1c0a0ed7dc040444d
Garrett Yamada <acutech@MBB-302-05.local>
2015-03-17 14:33:40 -0500

    Updated quests.php

--- quests.php
+++ quests.php
@@ -2,6 +2,7 @@
     $m = new MongoClient();
     $db = $m->selectDB("gamification_db");
     $collection = new MongoCollection( $db, "users-courses");
+    $collection2 = new MongoCollection( $db, "quests");
 ?>
 
 
@@ -160,6 +161,47 @@
 
                 <!-- Main content -->
                 <section class="content">
+                	<div class="row"> 
+                	    <div class="col-md-4" >
+                	   <!-- Button trigger modal -->
+                	    <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal"> Create New Quest </button>
+
+                	    <!-- Modal -->
+                	        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                	          <div class="modal-dialog">
+                	            <div class="modal-content">
+                	              <div class="modal-header">
+                	                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                	                <h4 class="modal-title" id="myModalLabel">New Quest</h4>
+                	              </div>
+                	              <div class="modal-body">
+                	                <!-- Create Quest Form -->
+                	                <form>
+                	                  <div class="form-group">
+                	                    <label>Title</label>
+                	                    <input type="text" class="form-control" placeholder="Quest Title">
+                	                  </div>
+                	                  <div class="form-group">
+                	                    <label>XP</label>
+                	                    <input type="number" class="form-control" placeholder="100">
+                	                  </div>
+                	                  <div class="form-group">
+                	                    <label>Description</label>
+                	                    <textarea class="form-control" rows="3"></textarea>
+                	                  </div>
+                	                </form>
+                	                <!-- /Create Quest Form -->
+                	              </div>
+                	              <div class="modal-footer">
+                	                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                	                <button type="button submit" class="btn btn-primary">Create</button>
+                	              </div>
+                	            </div>
+                	          </div>
+                	        </div>
+                	    </div>
+                	</div> <!-- /row -->
+                	<br>                
                     <div class="row">
                         <div class="col-xs-12">
                             <div class="box">
