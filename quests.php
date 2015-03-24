29ad7457fc2ac9c7e8027ed31b386fa32ccf1038
Garrett Yamada <gyamada619@gmail.com>
2015-03-24 14:32:45 -0500

    Adjusting database connections.

--- quests.php
+++ quests.php
@@ -170,12 +170,8 @@
 
                                 <?php
 
-                                	$m = new MongoClient();
-                                    $db = $m->selectDB("gamification_db");
-                                    $collection = new MongoCollection( $db, "quests");
-
-                                	$newquest=array('title' => $_POST["title"], 'xp' => $_POST["xp"], 'desc' => $_POST["desc"], 'course_id' => 'DET 210');
-                                	$collection->save($newquest);
+                                	$newquest=array('title' => $_POST["title"], 'xp' => $_POST["xp"], 'desc' => $_POST["desc"],);
+                                	$collection2->save($newquest);
 
                                 ?>
 
