f3b15548b9e7627196fe6ac3fc0bff1cfda4445d
Hailey Hendricks <hailey.kay.hendricks@gmail.com>
2015-04-07 14:47:35 -0500

    Making the date-picker work.

--- newquest.php
+++ newquest.php
@@ -4,7 +4,7 @@
     $db = $m->selectDB("gamification_db");
     $collection = new MongoCollection( $db, "quests");
 
-	$newquest=array('title' => $_POST["title"], 'xp' => $_POST["xp"], 'desc' => $_POST["desc"], 'course_id'=>"DET 210");
+	$newquest=array('title' => $_POST["title"], 'xp' => $_POST["xp"],  'due-date' => $_POST["due-date"], 'desc' => $_POST["desc"], 'course_id'=>"DET 210");
 	$collection->save($newquest);
 
 ?>
