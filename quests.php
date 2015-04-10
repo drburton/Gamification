97672f19685eb09fe7871a2f39c015d6a6472e0a
austingraham24 <austingraham24@gmail.com>
2015-04-10 01:58:16 -0500

    modal test

--- quests.php
+++ quests.php
@@ -206,7 +206,7 @@
                                             	      print "<td>$v</td>";
                                             	    }
 
-                                                print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#seedetails\">See Details</button></a></td>
+                                                print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#seedetails\" data-id=\"Test1\">See Details</button></a></td>
                                                 <td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#editquest\">Edit</button></a></td>
                                                 <td><a href=\"#\"><button class=\"btn btn-danger btn-sm\" data-toggle=\"modal\" data-target=\"#deletequest\">Delete</button></a></td>";
                                                 print "</tr>";
@@ -251,7 +251,7 @@
                  <div class="modal-content">
                    <div class="modal-header">
                      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                     <h4 class="modal-title" id="myModalLabel">Quest Details</h4>
+                     <h4 class="modal-title" id="detailsLabel">Quest Details</h4>
                    </div>
                    <div class="modal-body">
                      <h6>Title</h6>
@@ -367,5 +367,16 @@
           $('#date-picker2').datepicker({
           });
         </script>
+		<script type="text/javascript">
+			$('#seedetails').on('show.bs.modal', function (event) {
+			  var button = $(event.relatedTarget) // Button that triggered the modal
+			  var questId = button.data('id') // Extract info from data-* attributes
+			  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+			  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+			  var modal = $(this)
+			  modal.find('#detailsLabel').text(questId)
+			  <!--modal.find('.modal-body input').val(recipient)-->
+			})
+		</script>
     </body>
 </html>
