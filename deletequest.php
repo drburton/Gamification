d2c16d2d989f250ee9a266570b3dabf31f5a23ee
austingraham24 <austingraham24@gmail.com>
2015-04-10 03:12:47 -0500

    delete test 1

--- deletequest.php
+++ deletequest.php
@@ -4,7 +4,7 @@
     $db = $m->selectDB("gamification_db");
     $collection = new MongoCollection( $db, "quests");
 
-    $collection->remove(array('title'=>'something'));
+    $collection->remove(array('title'=>$_POST["deleteTitle"]));
 	
 	header("Location: http://gamedev.garrettyamada.com/quests.php");
 
