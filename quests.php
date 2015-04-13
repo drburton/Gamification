d4ca764c468ab4518b6300b8376af0b0606f69ac
austingraham24 <austingraham24@gmail.com>
2015-04-13 09:28:43 -0500

    not working

--- quests.php
+++ quests.php
@@ -443,7 +443,7 @@
 			  modal.find('#editDesc').val(questDesc)
 			  modal.find('#date-picker').val(questDue)
 			  $('#date-picker').datepicker(function(e){
-			  e.stopPropagation();
+			  //e.stopPropagation();
 			  this.focus();});
 			  //$("#datepicker").datepicker("setDate", questDue);
 			});
