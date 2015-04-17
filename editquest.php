25400c8560255c76b9929a3a8f20fe509075e9cc
austingraham24 <austingraham24@gmail.com>
2015-04-17 09:23:54 -0500

    editing editquest

--- editquest.php
+++ editquest.php
@@ -3,8 +3,11 @@
     $db = $m->selectDB("gamification_db");
     $collection2 = new MongoCollection( $db, "quests");
 
+    console.log(array("title" => $_POST["oldTitle"]))
+    console.log("This is a test!!!")
+
     $newquest=array('title' => $_POST["title"], 'xp' => $_POST["xp"],  'due_date' => $_POST["due_date"], 'desc' => $_POST["desc"], 'course_id'=>"DET 210");
-    $collection2->update(array("title" => $_POST["current"]),$newquest);
+    $collection2->update(array("title" => $_POST["oldTitle"]),$newquest);
 
     header("Location: http://gamedev.garrettyamada.com/quests.php");
 ?>
