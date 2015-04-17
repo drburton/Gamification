c82aac41340024867b0e95f6f7588fe7be9a0a8d
austingraham24 <austingraham24@gmail.com>
2015-04-17 09:24:53 -0500

    semicolons

--- editquest.php
+++ editquest.php
@@ -3,8 +3,8 @@
     $db = $m->selectDB("gamification_db");
     $collection2 = new MongoCollection( $db, "quests");
 
-    console.log(array("title" => $_POST["oldTitle"]))
-    console.log("This is a test!!!")
+    console.log(array("title" => $_POST["oldTitle"]));
+    console.log("This is a test!!!");
 
     $newquest=array('title' => $_POST["title"], 'xp' => $_POST["xp"],  'due_date' => $_POST["due_date"], 'desc' => $_POST["desc"], 'course_id'=>"DET 210");
     $collection2->update(array("title" => $_POST["oldTitle"]),$newquest);
