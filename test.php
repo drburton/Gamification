1e7903e46161b33579794eca231fd460b9b21eb2
austingraham24 <austingraham24@gmail.com>
2015-04-20 10:52:05 -0500

    testing test.php

--- test.php
+++ test.php
@@ -4,7 +4,8 @@
     $collection = new MongoCollection( $db, "users-courses");
     $collection2 = new MongoCollection( $db, "quests");
     $course=$_GET["course"];
-    console.log($course);
+    console.log(array("course" => $_GET["course"]));
+    console.log("This is a test!!!");
 
-    header("Location: http://gamedev.garrettyamada.com/quests.php");
+    //header("Location: http://gamedev.garrettyamada.com/quests.php");
 ?>
