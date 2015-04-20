9b3e276d9bdc84bcd860f0fefdd239e256ca19db
austingraham24 <austingraham24@gmail.com>
2015-04-20 17:19:26 -0500

    checking for \n

--- editquest.php
+++ editquest.php
@@ -4,10 +4,11 @@
     $collection2 = new MongoCollection( $db, "quests");
 
     console.log(array("title" => $_POST["oldTitle"]));
-    console.log("This is a test!!!");
+    print($_POST["due_date"]);
+    print"This is a \ntest!!!";
 
     $newquest=array('$set'=>array('title' => $_POST["title"], 'xp' => (int)$_POST["xp"],  'due_date' => $_POST["due_date"], 'desc' => $_POST["desc"], 'course_id'=>"DET 210"));
     $collection2->update(array("title" => $_POST["oldTitle"]),$newquest);
 
-    header("Location: http://gamedev.garrettyamada.com/quests.php");
+    //header("Location: http://gamedev.garrettyamada.com/quests.php");
 ?>
--- test.php
+++ test.php
@@ -32,24 +32,131 @@
         <![endif]-->
     </head>
     <body class="skin-blue">
-        <?php
-            $results = array('course_id' => 'jad00a');
-            $count=1;
-            $cursor = $collection->find($results);
-            $cursor->fields(array("course_id" => true));
-            foreach ($cursor as $doc) {
-                foreach ($doc as $k => $v) {
-                    if($count%2==0){
-                        $new = str_replace(" ","_",$v);
-                        print "<li><a href=\"test.php?course=" . $new . "\"><i class=\"fa fa-angle-double-right\"></i>$v</a></li>";
-                    }
+                <!-- header logo: style can be found in header.less -->
+                <header class="header">
+            <a href="dashboard.php" class="logo" >
+                <!-- Add the class icon to your logo image or logo icon to add the margining -->
+                EduQuest
+            </a>
+            <!-- Header Navbar: style can be found in header.less -->
+            <nav class="navbar navbar-static-top" role="navigation" >
+                <!-- Sidebar toggle button-->
+                <a href="#" class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                <div class="navbar-right">
+                    <ul class="nav navbar-nav">
 
-                    $count++;
+                        <!-- User Account: style can be found in dropdown.less -->
+                        <li class="dropdown user user-menu">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="glyphicon glyphicon-user"></i>
+                                <span>Garrett Yamada <i class="caret"></i></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <!-- User image -->
+                                <li class="user-header">
+                                    <img src="img/avatar5.png" class="img-circle" alt="User Image" />
+                                    <p>
+                                        Garrett Yamada
+                                        <small>Level 25 Web Developer</small>
+                                    </p>
+                                </li>
+                                <!-- Menu Body -->
 
-                }
+                                <!-- Menu Footer-->
+                                <li class="user-footer">
+                                    <div class="pull-left">
+                                        <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <!-- /header -->
 
-            }
-            print"<h1>Test</h1>";
-    		print"<p>$course</p>";;
+        <div class="wrapper row-offcanvas row-offcanvas-left">
+            <!-- Left side column. contains the logo and sidebar -->
+            <aside class="left-side sidebar-offcanvas">
+                <!-- sidebar: style can be found in sidebar.less -->
+                <section class="sidebar">
+                    <!-- Sidebar user panel -->
+                    <div class="user-panel">
+                        <div class="pull-left image">
+                            <img src="img/avatar5.png" class="img-circle" alt="User Image" />
+                        </div>
+                        <div class="pull-left info">
+                            <p>Greetings, Garrett!</p>
+
+                            <a href="#"><i class="fa fa-trophy"></i> Web Developer</a>
+                        </div>
+                    </div> <!-- /Sidebar user panel -->
+
+                    <!-- sidebar menu: : style can be found in sidebar.less -->
+                    <ul class="sidebar-menu">
+                        <li class="active">
+                            <a href="dashboard.php">
+                                <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                            </a>
+                        </li>
+                        <li class="treeview">
+                            <a href="#">
+                                <i class="fa fa-university"></i>
+                                <span>My Courses</span>
+                            </a>
+                            <ul class="treeview-menu">
+                              <?php
+
+                                  $results = array('user_id' => 'jad00a');
+                                  $count=1;
+                                  $cursor = $collection->find($results);
+                                  $cursor->fields(array("course_id" => true));
+                                  foreach ($cursor as $doc) {
+
+                                    foreach ($doc as $k => $v) {
+
+                                      if($count%2==0){
+                                        $new = str_replace(" ","_",$v);
+                                        print "<li><a href=\"test.php?course=" . $new . "\"><i class=\"fa fa-angle-double-right\"></i>$v</a></li>";
+                                      }
+
+                                      $count++;
+
+                                    }
+
+                                  }
+
+                               ?>
+
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-user"></i> <span>Profile</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-gear"></i> <span>Settings</span>
+                            </a>
+                        </li>
+
+                    </ul>
+                </section>
+                <!-- /.sidebar -->
+            </aside>
+            <?php
+                print"<h1>Test</h1>";
+                print"<p>$course</p>";
+            ?>
     </body>
     </html>
