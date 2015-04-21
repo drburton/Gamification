182f5006aad66b6489e84fa936181550f5812d5a
austingraham24 <austingraham24@gmail.com>
2015-04-20 21:26:05 -0500

    By Jove I think i got it!

--- editquest.php
+++ editquest.php
@@ -2,13 +2,13 @@
     $m = new MongoClient();
     $db = $m->selectDB("gamification_db");
     $collection2 = new MongoCollection( $db, "quests");
-    print $_POST["desc"];
+    //print $_POST["desc"];
     $newDesc = $_POST["desc"];
     $newDesc = str_replace("\r\n",'**',$newDesc);
-    print $newDesc;
+    //print $newDesc;
 
     $newquest=array('$set'=>array('title' => $_POST["title"], 'xp' => (int)$_POST["xp"],  'due_date' => $_POST["due_date"], 'desc' => $newDesc, 'course_id'=>"DET 210"));
     $collection2->update(array("title" => $_POST["oldTitle"]),$newquest);
 
-    //header("Location: http://gamedev.garrettyamada.com/quests.php");
+    header("Location: http://gamedev.garrettyamada.com/quests.php");
 ?>
--- quests.php
+++ quests.php
@@ -413,6 +413,7 @@
                 }
                 while (questDesc.search("_")!=-1){
                     questDesc=questDesc.replace("_"," ")
+                    questDesc=questDesc.replace("**","\r\n")
                 }
 			  //console.log(questDesc)
 			  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
