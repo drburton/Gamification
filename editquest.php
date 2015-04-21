69102ac5afa1d8c6d489794996c5b7c163b31279
austingraham24 <austingraham24@gmail.com>
2015-04-20 20:45:49 -0500

    new line again

--- editquest.php
+++ editquest.php
@@ -4,8 +4,10 @@
     $collection2 = new MongoCollection( $db, "quests");
 
     console.log(array("title" => $_POST["oldTitle"]));
-    print'$_POST["desc"]';
-    print"This is a \ntest!!!";
+    print$_POST["desc"];
+    
+    $line=array("This is a \ntest!!!");
+    print "<p>$line</p>"
 
     $newquest=array('$set'=>array('title' => $_POST["title"], 'xp' => (int)$_POST["xp"],  'due_date' => $_POST["due_date"], 'desc' => $_POST["desc"], 'course_id'=>"DET 210"));
     $collection2->update(array("title" => $_POST["oldTitle"]),$newquest);
