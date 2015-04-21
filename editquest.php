885ea23fd6f9d68f867eda5e22c1f4347366e99a
austingraham24 <austingraham24@gmail.com>
2015-04-20 20:51:44 -0500

    test2

--- editquest.php
+++ editquest.php
@@ -5,9 +5,6 @@
 
     console.log(array("title" => $_POST["oldTitle"]));
     print$_POST["desc"];
-    
-    $line=array("This is a \ntest!!!");
-    print "<p>$line</p>";
 
     $newquest=array('$set'=>array('title' => $_POST["title"], 'xp' => (int)$_POST["xp"],  'due_date' => $_POST["due_date"], 'desc' => $_POST["desc"], 'course_id'=>"DET 210"));
     $collection2->update(array("title" => $_POST["oldTitle"]),$newquest);
