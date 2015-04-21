0101e602aa7f77b0a5a0d3b0f7646fca680ef4c7
austingraham24 <austingraham24@gmail.com>
2015-04-21 16:08:49 -0500

    take2

--- config.php
+++ config.php
@@ -8,10 +8,10 @@ $coll = new MongoCollection( $db, "users");
 // $db   = $m->test;
 // $coll = $db->users;
 //include_once("functions.php");
-session_start();
-session_register("login");
-session_register("password");
-session_register("loggedIn");
+//session_start();
+//session_register("login");
+//session_register("password");
+//session_register("loggedIn");
 ?>
 <!DOCTYPE html>
 <html>
