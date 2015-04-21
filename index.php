da957be74d3ce32b56535e590b27fea9f146aab0
austingraham24 <austingraham24@gmail.com>
2015-04-21 16:00:27 -0500

    trying to update code

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    //include_once("config.php");
+    include_once"config.php";
     //if (loggedIn()){
     //    header("Location: http://gamedev.garrettyamada.com/dashboard.php");
     //}
