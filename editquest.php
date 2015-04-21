e1bc2b18c8e1145e0f4eca57f836ad9707bdf011
austingraham24 <austingraham24@gmail.com>
2015-04-20 21:15:09 -0500

    semicolon

--- editquest.php
+++ editquest.php
@@ -5,7 +5,7 @@
     print $_POST["desc"];
     $newDesc = $_POST["desc"];
     $newDesc = str_replace('\r\n','**',$newDesc);
-    print $newDesc
+    print $newDesc;
 
     $newquest=array('$set'=>array('title' => $_POST["title"], 'xp' => (int)$_POST["xp"],  'due_date' => $_POST["due_date"], 'desc' => $newDesc, 'course_id'=>"DET 210"));
     $collection2->update(array("title" => $_POST["oldTitle"]),$newquest);
