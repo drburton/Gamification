9856bd1b751400a4413d6d7ba09c0d045199e47e
austingraham24 <austingraham24@gmail.com>
2015-04-21 14:34:16 -0500

    break

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
                     </div>
                 </div>
                 <div class="footer">
-                    <a href="signUp.php" style="font-color:white;"><button type="button" class="btn bg-primary btn-block">Sign Up</button></a>
+                    <a href="signUp.php" style="font-color:white;"><button type="button" class="btn bg-primary btn-block">Sign Up</button></a><br/>
                     <button type="submit" class="btn bg-primary btn-block">Sign me in</button>
                 </div>
             </form>
