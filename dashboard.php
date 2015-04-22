274ad73b7c5d7c6fa86939e834eca0df2662b035
austingraham24 <austingraham24@gmail.com>
2015-04-22 17:19:25 -0500

    dashboard includes config

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,7 @@
     $m = new MongoClient();
     $db = $m->selectDB("gamification_db");
     $collection = new MongoCollection( $db, "users-courses");
-    console.log("Hello!")
+    include_once "config.php";
 ?>
 
 
