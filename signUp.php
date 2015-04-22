896917e78505cb6fd8f98d3b8e019222f0791f7f
austingraham24 <austingraham24@gmail.com>
2015-04-22 09:11:35 -0500

    back to original

--- signUp.php
+++ signUp.php
@@ -3,25 +3,25 @@ include_once("config.php");
 if(loggedIn()){
   header('Location: dashboard.php');
 }
-// if(isset($_POST["submit"])){
-// 	if(!($_POST["password"] == $_POST["password2"])){
-// 	 print "<p>Your passwords did not match</p>";
-//   }
-// 	else{
-//     $query = $coll->findOne(array('_id' => $_POST['login']));
-//   	if(empty($query)){
-//   	 	newUser($_POST["login"], $_POST["password"]);
-//   	 	cleanMemberSession($_POST["login"], $_POST["password"]);
-//   	 	header("Location: members.php");
-//     }
-//   	else{
-//   	  print '<p>Username already exists, please choose another username.</p>';
-//   	}
-//   }
+if(isset($_POST["submit"])){
+	if(!($_POST["password"] == $_POST["password2"])){
+	 print "<p>Your passwords did not match</p>";
+  }
+	else{
+    $query = $coll->findOne(array('_id' => $_POST['login']));
+  	if(empty($query)){
+  	 	newUser($_POST["login"], $_POST["password"]);
+  	 	cleanMemberSession($_POST["login"], $_POST["password"]);
+  	 	header("Location: members.php");
+    }
+  	else{
+  	  print '<p>Username already exists, please choose another username.</p>';
+  	}
+  }
 }
 ?>
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html class="bg-black">
     <head>
         <meta charset="UTF-8">
@@ -29,39 +29,15 @@ if(loggedIn()){
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <!-- Theme style -->
+        <!-- Theme style --
         <link href="css/AdminLTE.css" rel="stylesheet" type="text/css" />
 
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-        <![endif]-->
     </head>
     <body class="bg-black">
 
         <div class="form-box" id="login-box">
 
             <div class="header">EduQuest</div>
-            <?php
-              if(isset($_POST["submit"])){
-                if(!($_POST["password"] == $_POST["password2"])){
-                  print "<p>Your passwords did not match</p>";
-                }
-                else{
-                  $query = $coll->findOne(array('_id' => $_POST['login']));
-                  if(empty($query)){
-                  newUser($_POST["login"], $_POST["password"]);
-                  cleanMemberSession($_POST["login"], $_POST["password"]);
-                  header("Location: members.php");
-                  }
-                  else{
-                    print '<p>Username already exists, please choose another username.</p>';
-                  }
-                }
-              }
-            ?>
             <form action="<?=$_SERVER["PHP_SELF"];?>" method="POST">
 
                 <div class="body bg-gray">
@@ -87,11 +63,11 @@ if(loggedIn()){
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js" type="text/javascript"></script>
 
     </body>
-</html>
+</html> -->
 
 
 <!--Old html-->
-<!--
+
 <html>
 <head>
   <title>Simple Authentication with MongoDB</title>
@@ -134,4 +110,4 @@ if(loggedIn()){
 </table>
 </form>
 </body>
-</html>-->
+</html>
