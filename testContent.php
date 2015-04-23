0b2e70f5ff63b14064a26af27a5a4b02db94673c
austingraham24 <austingraham24@gmail.com>
2015-04-23 14:23:49 -0500

    now include the testDashboard

--- testContent.php
+++ testContent.php
@@ -1,11 +1,11 @@
 <?php
     // if (!loggedIn()){
-    //     header("Location: /index.php");
+    //     header("Location: index.php");
     // }
     $m = new MongoClient();
     $db = $m->selectDB("gamification_db");
     $collection = new MongoCollection( $db, "users-courses");
-    //include_once "testDash.php";
+    include_once "testDash.php";
 ?>
 <html>
 <head>
