5c812c4d41a805d2f3ed4375903ddd088b069d75
austingraham24 <austingraham24@gmail.com>
2015-04-23 14:55:04 -0500

    moved it again

--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,11 @@
 <?php
-    // if (!loggedIn()){
-    //     header("Location: /index.php");
-    // }
-
     $m = new MongoClient();
     $db = $m->selectDB("gamification_db");
     $collection = new MongoCollection( $db, "users-courses");
     //include_once "navTemplate.php";
+    if (!loggedIn()){
+        header("Location: /index.php");
+    }
 ?>
 <!DOCTYPE html>
 <html>
