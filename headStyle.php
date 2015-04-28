8ee0642ef580ce459f9fa144fc5cc6e5844a82ac
austingraham24 <austingraham24@gmail.com>
2015-04-28 16:28:04 -0500

    was admin lte always overriding style?

--- headStyle.php
+++ headStyle.php
@@ -1,6 +1,6 @@
 <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-<!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" /> -->
-<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"><!-- for enhanced bootstrap theming--> -->
+<!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">--><!-- for enhanced bootstrap theming-->
 <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
 <!-- Date Picker -->
 <link href="css/bootstrap-datepicker3.css" rel="stylesheet" type="text/css" />
@@ -9,5 +9,5 @@
 <!-- bootstrap wysihtml5 - text editor -->
 <link href="css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" />
 <!-- Theme style -->
-<link href="css/AdminLTE.css" rel="stylesheet" type="text/css" />
+<!-- <link href="css/AdminLTE.css" rel="stylesheet" type="text/css" /> -->
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
