bf124e5c84f482188317c9195ba57b8d6d433306
austingraham24 <austingraham24@gmail.com>
2015-04-28 15:52:12 -0500

    find those extra calls to files
    index had old head section and wasn't using the headStyle.php file

--- headStyle.php
+++ headStyle.php
@@ -1,5 +1,6 @@
 <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"><!-- for enhanced bootstrap theming-->
 <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
 <!-- Date Picker -->
 <link href="css/bootstrap-datepicker3.css" rel="stylesheet" type="text/css" />
