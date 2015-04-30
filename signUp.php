40b8b07b4893e8fdc1dc27a5560cd1d3bfd55e18
austingraham24 <austingraham24@gmail.com>
2015-04-30 14:43:35 -0500

    min length

--- signUp.php
+++ signUp.php
@@ -62,7 +62,7 @@ if(isset($_POST["submit"])){
                   ?>
                     <div class="form-group">
                         <input type="text" name="login" class="form-control" placeholder="ACU ID (abc12d)"
-                        value="<?php print isset($_POST["login"]) ? $_POST["login"] : "" ; ?>"maxlength="6" required="true">
+                        value="<?php print isset($_POST["login"]) ? $_POST["login"] : "" ; ?>" minlenght="6" maxlength="6" required="true">
                     </div>
                     <div class="form-group">
                         <input type="text" name="name" class="form-control" placeholder="Your Name"
