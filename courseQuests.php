9ea05219eb854dab421e1d24942d9fb0329839de
austingraham24 <austingraham24@gmail.com>
2015-04-30 13:40:13 -0500

    removing the empty print

--- courseQuests.php
+++ courseQuests.php
@@ -123,7 +123,6 @@
                                                 print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#acceptquest\" data-course=$Ucourse data-id=$title data-due=$due_date data-xp=$xp data-desc=$desc>Accept Quest</button></a></td>";
                                                 print "</tr>";
                                                 }
-                                              print "<p>".empty($courseCursor)"</p";
                                              ?>
 
                                     </table>
