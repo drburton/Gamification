c0d0da4b4716454b6dab1c187be47a7eff56ba03
austingraham24 <austingraham24@gmail.com>
2015-04-30 13:34:34 -0500

    adding user name to the database entry

--- acceptquest.php
+++ acceptquest.php
@@ -5,10 +5,11 @@
     //print $_POST["desc"];
     $newDesc = $_POST["desc"];
     $newDesc = str_replace("\r\n",'~',$newDesc);
+    $name=$_SESSION["login"]
     //print $newDesc;
 
     $newquest=array('title' => $_POST["title"], 'xp' => (int)$_POST["xp"],  'due_date' => $_POST["due_date"], 
-        'desc' => $newDesc, 'course_id'=>$_POST["course"]);
+        'desc' => $newDesc, 'course_id'=>$_POST["course"], 'user-id'=>$name);
     $collection2->save($newquest);
 
     header("Location: http://gamedev.garrettyamada.com/courseQuests.php?course=".$_POST["course"]);//Kick user back to quests page after inputting new quest into database
