25daad44821d4cc74dc00a09fd7cb355454089b2
austingraham24 <austingraham24@gmail.com>
2015-04-30 22:19:14 -0500

    fixing accept quest to input only given fields

--- acceptquest.php
+++ acceptquest.php
@@ -9,8 +9,7 @@
     $name=$_SESSION["login"];
     //print $newDesc;
 
-    $newquest=array('title' => $_POST["title"], 'xp' => (int)$_POST["xp"],  'due_date' => $_POST["due_date"], 
-        'desc' => $newDesc, 'course_id'=>$_POST["course"], 'user-id'=>$name);
+    $newquest=array('title' => $_POST["title"], 'course_id'=>$_POST["course"], 'user-id'=>$name);
     $collection2->save($newquest);
 
     header("Location: http://gamedev.garrettyamada.com/courseQuests.php?course=".$_POST["course"]);//Kick user back to quests page after inputting new quest into database
