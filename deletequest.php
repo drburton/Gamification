f28c8b1351a813954c27cfe8cfb9bdc9888b67e5
austingraham24 <austingraham24@gmail.com>
2015-05-05 00:26:59 -0500

    making sure delete quest doesnt delete quests of the same name in other classes

--- deletequest.php
+++ deletequest.php
@@ -1,11 +1,11 @@
 <?php
 
-  $m = new MongoClient();
+		$course=$_POST["course"];
+    $new = str_replace(" ","_",$course);
+  	$m = new MongoClient();
     $db = $m->selectDB("gamification_db");
     $collection = new MongoCollection( $db, "quests");
-    $collection->remove(array('title'=>$_POST["deleteTitle"]));
-    $course=$_POST["course"];
-    $new = str_replace(" ","_",$course);
+    $collection->remove(array('title'=>$_POST["deleteTitle"],'course_id'=>$course));
 	
 	header("Location: http://gamedev.garrettyamada.com/quests.php?course=".$new);
 
