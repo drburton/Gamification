2738b7633e3dfe9c78f1b21755a89b7c1ccc469a
Austin Graham <austingraham24@gmail.com>
2016-04-26 14:29:33 -0500

    using cursor 2

--- courseQuests.php
+++ courseQuests.php
@@ -172,7 +172,7 @@
                                             $xp="";
                                             $desc='';
                                             $dbid="";
-                                            foreach ($cursor as $doc) { //Turn cursor (results) human readable
+                                            foreach ($cursor2 as $doc) { //Turn cursor (results) human readable
                                               print "<tr>";
                                               foreach ($doc as $k => $v) { //Filter out keys from key-value pairs in the returned array
                                                 if ($k != "desc"){
