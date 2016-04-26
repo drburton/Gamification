c03d3a2fd2aa034410418063f85b21d570af71e5
Austin Graham <austingraham24@gmail.com>
2016-04-26 14:28:49 -0500

    duplicate table

--- courseQuests.php
+++ courseQuests.php
@@ -152,41 +152,53 @@
                               </div><!-- /.box-header -->
                               <div class="box-body table-responsive no-padding">
                                   <table class="table table-hover">
-                                    <thead>
                                       <tr>
                                           <th>Quest</th>
                                           <th>XP</th>
                                           <th>Due Date</th>
                                           <th>Details</th>
-                                          <th>Drop</th>
+                                          <th>Accept</th>
                                       </tr>
 
                                       <!-- PHP to pull quest data and put in table -->
                                           <?php
-                                              $results = array('course_id' => $course,'user_id'=>$_SESSION['login']);//$course);
-                                              $cursor2 = $collection4->find($results);
-                                              $cursor2->fields(array("title" => true,'_id' => false)); //Get specific data 
-                                              echo count("<tr><td>".$cursor2."</td></tr>");
-                                              $due_date="";
-                                              $xp="";
-                                              $desc='';
-                                              $dbid="";
-                                              $title="";
-                                              foreach ($cursor as $doc) { //Turn cursor (results) human readable
+
+                                            $results = array('course_id' => $course);//$course);
+                                            $cursor = $collection2->find($results); //Return a quest result set
+                                            $cursor->fields(array("title" => true, 'due_date' => true, 'xp' => true, 'desc' => true, '_id' => false)); //Get specific data
+                                            $cursor=$cursor->sort(array("title"=>1)); //Sort by title
+                                            $title="";
+                                            $due_date="";
+                                            $xp="";
+                                            $desc='';
+                                            $dbid="";
+                                            foreach ($cursor as $doc) { //Turn cursor (results) human readable
                                               print "<tr>";
                                               foreach ($doc as $k => $v) { //Filter out keys from key-value pairs in the returned array
-
-                                                print "<td>$v</td>";
-
+                                                if ($k != "desc"){
+                                                  if($k=="title"){
+                                                    $title=$v;
+                                                  }
+                                                  if($k=="due_date"){
+                                                    $due_date=$v;
+                                                  }
+                                                  if($k=="xp"){
+                                                    $xp=$v;
+                                                  }
+                                                  print "<td>$v</td>";
                                                 }
-                                                //$title = str_replace(" ","_",$title);
-                                                //$desc = str_replace(" ","_",$desc);
+                                                else{
+                                                  $desc=$v;
+                                                }
+                                                }
+                                                $title = str_replace(" ","_",$title);
+                                                $desc = str_replace(" ","_",$desc);
                                                 //print "<td>$desc</td>";
 
-                                                /*print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#seedetails\" data-id=$title data-due=$due_date data-xp=$xp data-desc=$desc>See Details</button></a></td>";
-                                                print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#acceptquest\" data-course=$Ucourse data-id=$title>Accept Quest</button></a></td>";
-                                                */print("</tr>");
-                                                }
+                                              print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#seedetails\" data-id=$title data-due=$due_date data-xp=$xp data-desc=$desc>See Details</button></a></td>";
+                                              print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#acceptquest\" data-course=$Ucourse data-id=$title>Accept Quest</button></a></td>";
+                                              print "</tr>";
+                                              }
                                            ?>
 
                                   </table>
