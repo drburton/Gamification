33633aa7dad8647a3bdde8c89667f977a157d3da
austingraham24 <austingraham24@gmail.com>
2016-06-06 11:41:52 -0500

    debugging accepted quest list

--- courseQuests.php
+++ courseQuests.php
@@ -196,6 +196,8 @@
                                                 $desc = str_replace(" ","_",$desc);
                                                 //print "<td>$desc</td>";
 
+                                              print("<td>Item</td>");
+                                              print("<td>Item</td>");
                                               print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#seedetails\" data-id=$title data-due=$due_date data-xp=$xp data-desc=$desc>See Details</button></a></td>";
                                               print "<td><a href=\"#\"><button class=\"btn btn-alert btn-sm\" data-toggle=\"modal\" data-target=\"#dropquest\" data-course=$Ucourse data-id=$title>Drop Quest</button></a></td>";
                                               print "</tr>";
