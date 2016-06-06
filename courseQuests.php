934667773be4f3384a9a1ef6affb694e77a8a0f5
austingraham24 <austingraham24@gmail.com>
2016-06-06 12:52:48 -0500

    testing again

--- courseQuests.php
+++ courseQuests.php
@@ -136,11 +136,11 @@
                                                 $desc = str_replace(" ","_",$desc);
                                                 //print "<td>$desc</td>";
 
-                                              //if(!in_array($title, $curQuests){
-                                              	print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#seedetails\" data-id=$title data-due=$due_date data-xp=$xp data-desc=$desc>See Details</button></a></td>";
-                                              	print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#acceptquest\" data-course=$Ucourse data-id=$title>Accept Quest</button></a></td>";
-                                              	print "</tr>";
-                                          	  //}
+	                                              if(!in_array($title, $curQuests){
+	                                              	print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#seedetails\" data-id=$title data-due=$due_date data-xp=$xp data-desc=$desc>See Details</button></a></td>";
+	                                              	print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#acceptquest\" data-course=$Ucourse data-id=$title>Accept Quest</button></a></td>";
+	                                              	print "</tr>";
+	                                          	  }
                                               }
                                            ?>
 
@@ -190,7 +190,6 @@
                                                   if($k=="xp"){
                                                     $xp=$v;
                                                   }
-                                                  echo ($v);
                                                   print "<td>$v</td>";
                                                 }
                                                 else{
