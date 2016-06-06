966f7db0e0d64d5ee30c02cf9b4104e8af130be6
austingraham24 <austingraham24@gmail.com>
2016-06-06 12:53:58 -0500

    missing parenthesis

--- courseQuests.php
+++ courseQuests.php
@@ -136,7 +136,7 @@
                                                 $desc = str_replace(" ","_",$desc);
                                                 //print "<td>$desc</td>";
 
-	                                              if(!in_array($title, $curQuests){
+	                                              if(!in_array($title, $curQuests)){
 	                                              	print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#seedetails\" data-id=$title data-due=$due_date data-xp=$xp data-desc=$desc>See Details</button></a></td>";
 	                                              	print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#acceptquest\" data-course=$Ucourse data-id=$title>Accept Quest</button></a></td>";
 	                                              	print "</tr>";
