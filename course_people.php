17a281c479c93ded81ac38a9c600c4021aa48d2a
austingraham24 <austingraham24@gmail.com>
2016-06-20 16:30:10 -0500

    more links for course nav

--- course_people.php
+++ course_people.php
@@ -77,6 +77,8 @@
                     <div class="navbar-left">
                         <ul class="nav navbar-nav">
                             <li><a href="#"><i class="fa fa-users"></i> People</a></li>
+                            <li><a href="#"><i class="fa fa-book"></i> Grades</a></li>
+                            <li><a href="#"><i class="fa fa-bookmark"></i> Quests</a></li>
                         </ul>
                     </div>
                 </nav>
