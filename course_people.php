0bc1669d736a2f630de7c6c6cce26e8e6462c75d
austingraham24 <austingraham24@gmail.com>
2016-06-20 16:43:38 -0500

    more complete nav section

--- course_people.php
+++ course_people.php
@@ -76,9 +76,10 @@
                 <nav class="navbar course-nav navbar-default navbar-collapse" style="margin:0px;">
                     <div class="navbar-left">
                         <ul class="nav navbar-nav">
-                            <li><a href="#"><i class="fa fa-users"></i> People</a></li>
+                            <li><a href="#"><i class="fa fa-exclamation"></i> Announcements</a></li>
                             <li><a href="#"><i class="fa fa-book"></i> Grades</a></li>
                             <li><a href="#"><i class="fa fa-bookmark"></i> Quests</a></li>
+                            <li><a href="#"><i class="fa fa-users"></i> People</a></li>
                         </ul>
                     </div>
                 </nav>
