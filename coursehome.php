476a8f00324d738277912c166fef725457f2d210
austingraham24 <austingraham24@gmail.com>
2016-06-20 17:01:25 -0500

    course home link

--- coursehome.php
+++ coursehome.php
@@ -76,6 +76,7 @@
                 <nav class="navbar course-nav navbar-default navbar-collapse" style="margin:0px;">
                     <div class="navbar-left">
                         <ul class="nav navbar-nav">
+                        	<li><a href="/coursehome.php?course=<?php print($course_under);?>"><i class="fa fa-home"></i> Home</a></li>
                             <li><a href="#"><i class="fa fa-exclamation"></i> Announcements</a></li>
                             <li><a href="#"><i class="fa fa-book"></i> Grades</a></li>
                             <li><a href="/courseQuests.php?course=<?php print($course_under);?>"><i class="fa fa-bookmark"></i> Quests</a></li>
