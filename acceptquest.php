c2a2133df1601d1dd9ce6016c7939912dd69b103
austingraham24 <austingraham24@gmail.com>
2016-06-21 13:12:50 -0500

    I need a quest in to test both

--- acceptquest.php
+++ acceptquest.php
@@ -1,6 +1,6 @@
 <?php
     include_once "config.php";
-    if(isset($_POST["action"])){
+    /*if(isset($_POST["action"])){
         $m = new MongoClient();
         $db = $m->selectDB("gamification_db");
         $collection2 = new MongoCollection( $db, "users-quests");
@@ -19,14 +19,15 @@
             $collection2->save($newquest);
 
             //print("/courseQuests.php?course=".$course);
-            header("Location: /courseQuests.php?course=".$course);*///Kick user back to quests page after inputting new quest into database
+            header("Location: /courseQuests.php?course=".$course);//Kick user back to quests page after inputting new quest into database
 
         }else{
             echo("null"); 
         }
     }else{
-    }
-    /*$m = new MongoClient();
+        echo("Bad Wolf");
+    }*/
+    $m = new MongoClient();
     $db = $m->selectDB("gamification_db");
     $collection2 = new MongoCollection( $db, "users-quests");
     //print $_POST["desc"];
@@ -41,5 +42,5 @@
     $collection2->save($newquest);
 
     //print("/courseQuests.php?course=".$course);
-    header("Location: /courseQuests.php?course=".$course);*///Kick user back to quests page after inputting new quest into database
+    header("Location: /courseQuests.php?course=".$course);//Kick user back to quests page after inputting new quest into database
 ?>
