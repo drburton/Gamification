34ff11c9c4af1e03fe677e663732f672f5a78fa5
austingraham24 <austingraham24@gmail.com>
2016-06-21 13:10:31 -0500

    accept quest functionality put back in

--- acceptquest.php
+++ acceptquest.php
@@ -1,17 +1,30 @@
 <?php
     include_once "config.php";
-    echo("Something\n");
     if(isset($_POST["action"])){
-        /*if($_POST("action")=="drop"){
+        $m = new MongoClient();
+        $db = $m->selectDB("gamification_db");
+        $collection2 = new MongoCollection( $db, "users-quests");
+
+        if($_POST("action")=="drop"){
             echo("Drop");
         }elseif($_POST("action")=="accept"){
-           echo("accept"); 
+            $newDesc = $_POST["desc"];
+            $newDesc = str_replace("\r\n",'~',$newDesc);
+            $name=$_SESSION["login"];
+            $course=$_POST["course"];
+            //print $newDesc;
+            $new = str_replace("_"," ",$course);
+
+            $newquest=array('title' => $_POST["title"], 'course_id'=>$new, 'user_id'=>$name);
+            $collection2->save($newquest);
+
+            //print("/courseQuests.php?course=".$course);
+            header("Location: /courseQuests.php?course=".$course);*///Kick user back to quests page after inputting new quest into database
+
         }else{
             echo("null"); 
-        }*/
-        echo("action!");
+        }
     }else{
-        echo("no action"); 
     }
     /*$m = new MongoClient();
     $db = $m->selectDB("gamification_db");
