b5d07556f865d0de3954b9b69d0e274ab490cd73
austingraham24 <austingraham24@gmail.com>
2016-06-21 13:29:03 -0500

    testing the drop feature

--- acceptquest.php
+++ acceptquest.php
@@ -7,6 +7,15 @@
 
         if($_POST["action"]=="drop"){
             echo("Drop");
+            $name=$_SESSION["login"];
+            $course=$_POST["course"];
+            $new = str_replace("_"," ",$course);
+
+            $newquest=array('title' => $_POST["title"], 'course_id'=>$new, 'user_id'=>$name);
+            $collection2->remove($newquest);
+
+            //print("/courseQuests.php?course=".$course);
+            header("Location: /courseQuests.php?course=".$course);//Kick user back to quests page after removing quest from database
         }elseif($_POST["action"]=="accept"){
             $newDesc = $_POST["desc"];
             $newDesc = str_replace("\r\n",'~',$newDesc);
@@ -21,7 +30,7 @@
             //print("/courseQuests.php?course=".$course);
             header("Location: /courseQuests.php?course=".$course);//Kick user back to quests page after inputting new quest into database
         }else{
-            echo("null"); 
+            header("Location: /courseQuests.php?course=".$course); //not a valid action so send them back 
         }
     }else{
         echo("Bad Wolf");
@@ -42,5 +51,6 @@
     $collection2->save($newquest);
 
     //print("/courseQuests.php?course=".$course);
-    header("Location: /courseQuests.php?course=".$course);*/ //Kick user back to quests page after inputting new quest into database
+    header("Location: /courseQuests.php?course=".$course); //Kick user back to quests page after inputting new quest into database
+    */
 ?>
