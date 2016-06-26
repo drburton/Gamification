8b04276e88e269b8a6b810126fb24a511b57e0a7
austingraham24 <austingraham24@gmail.com>
2016-06-26 13:36:19 -0500

    smaller box

--- profile.php
+++ profile.php
@@ -30,7 +30,7 @@
                 <!-- Main content -->
                 <section class="content" style="background-image: url(img/wood4.png); background-repeat: repeat; height:100vh;">
 
-                <div class="col-md-8">
+                <div class="col-md-6">
                     <div class="box">
                         <div class="box-header">
                             <h3 class="box-title">My Stats:</h3>
