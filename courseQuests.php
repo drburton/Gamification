d463908f95428e04ca375426bac5a545a75806a1
Austin Graham <austingraham24@gmail.com>
2016-07-10 15:55:36 -0500

    didn't like in_array

--- courseQuests.php
+++ courseQuests.php
@@ -142,8 +142,7 @@
                                                 }
                                                 $title = str_replace(" ","_",$title);
                                                 $desc = str_replace(" ","_",$desc);
-                                                //print($dbid->{"$id"});
-                                                print(!in_array($dbid, $curQuests)));
+                                                print($dbid->{"$id"});
 
 	                                              if(!in_array($title, $curQuests)){
 	                                              	print "<td><a href=\"#\"><button class=\"btn btn-default btn-sm\" data-toggle=\"modal\" data-target=\"#seedetails\" data-id=$title data-due=$due_date data-xp=$xp data-desc=$desc>See Details</button></a></td>";
