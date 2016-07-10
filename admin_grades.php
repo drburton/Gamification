7178305d90222788ade6dc3ad62f236d1a3b041a
Austin Graham <austingraham24@gmail.com>
2016-07-10 11:54:10 -0500

    starting grades table

--- admin_grades.php
+++ admin_grades.php
@@ -1,1 +1,20 @@
-<h1>Test</h1>
+<div class="box">
+                              <div class="box-header">
+                                  <h3 class="box-title">Available Quests</h3>
+                              </div><!-- /.box-header -->
+                              <div class="box-body table-responsive no-padding">
+                                  <table class="table table-hover">
+                                  <thead>
+                                      <tr>
+                                          <th>Quest</th>
+                                          <th>XP</th>
+                                          <th>Due Date</th>
+                                          <th>Details</th>
+                                          <th>Accept</th>
+                                      </tr>
+                                  </thead>
+                                  <tbody>
+                                  </tbody>
+                                  </table>
+                              </div><!-- /.box-body -->
+                          </div><!-- /.box -->
