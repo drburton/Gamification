685e0245a0097bda98a8d051c404a9bc88bc1e2b
Austin Graham <austingraham24@gmail.com>
2016-07-10 13:02:14 -0500

    commenting out overflow test

--- admin_grades.php
+++ admin_grades.php
@@ -9,7 +9,7 @@
                 <th>Due Date</th>
                 <th>Details</th>
                 <th>Accept</th> -->
-            <th style="text-align:center; vertical-align:middle;">Users</th>
+            <th style="text-align:center; vertical-align:middle; min-width: 175px;">Users</th>
             <?php
             foreach ($questCursor as $doc) { //Turn cursor (results) human readable
               $title;
@@ -27,9 +27,9 @@
               print($title."<br/>".$exp." xp</th>");
             }
 
-            for ($i = 0; $i <= 15; $i++) {
+            /*for ($i = 0; $i <= 15; $i++) {
                 print("<th style='min-width: 150px;'>Overflow Test</th>");
-            }
+            }*/
 
             ?>
 
