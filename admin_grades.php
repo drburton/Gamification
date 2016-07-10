9df2b322ac77d0a8659a718c03867365695e339d
Austin Graham <austingraham24@gmail.com>
2016-07-10 11:55:38 -0500

    fixing code style

--- admin_grades.php
+++ admin_grades.php
@@ -1,20 +1,22 @@
-<div class="box">
-                              <div class="box-header">
-                                  <h3 class="box-title">Available Quests</h3>
-                              </div><!-- /.box-header -->
-                              <div class="box-body table-responsive no-padding">
-                                  <table class="table table-hover">
-                                  <thead>
-                                      <tr>
-                                          <th>Quest</th>
-                                          <th>XP</th>
-                                          <th>Due Date</th>
-                                          <th>Details</th>
-                                          <th>Accept</th>
-                                      </tr>
-                                  </thead>
-                                  <tbody>
-                                  </tbody>
-                                  </table>
-                              </div><!-- /.box-body -->
-                          </div><!-- /.box -->
+<div class="col-xs-12">
+  <div class="box">
+    <div class="box-header">
+        <h3 class="box-title">Available Quests</h3>
+    </div><!-- /.box-header -->
+    <div class="box-body table-responsive no-padding">
+        <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Quest</th>
+                <th>XP</th>
+                <th>Due Date</th>
+                <th>Details</th>
+                <th>Accept</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+        </table>
+    </div><!-- /.box-body -->
+  </div><!-- /.box -->
+</div>
