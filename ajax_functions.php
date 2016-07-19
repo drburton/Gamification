14a8df91c636f4c064b72a324fa32442df67cfa7
austingraham24 <austingraham24@gmail.com>
2016-07-19 14:42:04 -0500

    removing debug prints

--- ajax_functions.php
+++ ajax_functions.php
@@ -1,38 +1,38 @@
 <?php
-print("Page Loaded<br/>");
+//print("Page Loaded<br/>");
 $response = "";
 
 if(isset($_GET['action'])){
 	$action = $_GET["action"];
-	print($action."<br/>");
+	//print($action."<br/>");
 	if($action=="changeGrade"){
-		print("Getting Data <br/>");
+		//print("Getting Data <br/>");
 		$user = $_GET["user"];
 		$response=$response."_".$user;
 		$quest = $_GET["qId"];
 		$response=$response."_".$quest;
 		$grade = $_GET["grade"];
 		$response=$response."_".$grade;
-		print($response."<br/>");
-		changeGrade($user,$quest,$grade);
+		//print($response."<br/>");
+		changeGrade($user,$quest,(int)$grade);
 	}
 }
 function changeGrade($user,$questId,$grade){
-	print("DB Function <br/>");
-	print($user."_".$questId."_".$grade."<br/>");
+	//print("DB Function <br/>");
+	//print($user."_".$questId."_".$grade."<br/>");
 	$m = new MongoClient();
     $db = $m->selectDB("gamification_db");
     $user_quests = new MongoCollection( $db, "users-quests");
-    print_r(array('user_id' => $user, 'quest_id' => $questId));
-    print("<br/>");
+    //print_r(array('user_id' => $user, 'quest_id' => $questId));
+    //print("<br/>");
     $cursor = $user_quests->findOne(array('user_id' => $user, 'quest_id' => $questId));
     if($cursor){
-    	print("Updating <br/>");
+    	//print("Updating <br/>");
      	$update=array('$set'=>array('grade' => $grade, 'status' => 'graded'));
 		$user_quests->update(array('user_id' => $user, 'quest_id' => $questId),$update);
 		//header("Location: profile.php");
     }
 }
-print('End');
+//print('End');
 //return response;
 ?>
