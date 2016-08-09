63f84c74e437e7f5f3edd663708079261bb1e8f0
austingraham24 <austingraham24@gmail.com>
2016-08-09 12:00:39 -0500

    extra text

--- student_grades.php
+++ student_grades.php
@@ -36,7 +36,7 @@
 			  </div>
 			</div>
 			<div class="centered-text">
-				<p>EXP Required for Next Milestone (<?php print($letterMilestone);?>): <?php print($milestoneValue-$userXP);?></p>
+				<p>EXP Required for Next Milestone (<?php print($letterMilestone);?>): <?php print($milestoneValue-$userXP);?> points</p>
 			</div>
 		</div>
 	</div>
