e0ee73ac94d896e79023030f1fe4f048a2502b4b
austingraham24 <austingraham24@gmail.com>
2016-08-09 10:12:10 -0500

    still having test text

--- student_grades.php
+++ student_grades.php
@@ -1,6 +1,7 @@
 <div class="col-xs-12">
 	<div class="box">
 		<div>
+			<h1>Test</h1>
 			<div class="progress">
 			  <div class="progress-bar" role="progressbar" aria-valuenow="70"
 			  aria-valuemin="0" aria-valuemax="100" style="width:70%">
