04a70e7cebf5750bd52c71c047f84e0c1a206fb5
austingraham24 <austingraham24@gmail.com>
2016-08-09 10:35:08 -0500

    testing something

--- student_grades.php
+++ student_grades.php
@@ -7,9 +7,9 @@
 <div class="col-xs-12">
 	<div class="box">
 		<div>
-			<h1 class="centered-text">Course Progress: <?php print($userXP);?></h1>
+			<h1 class="centered-text">Course Progress: <?php print($userXP);?>XP</h1>
 			<div class="progress horizontal-centered" style="width:80%;">
-			  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="70"
+			  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="90"
 			  aria-valuemin="0" aria-valuemax="100" style="width:70%">
 			    70%
 			  </div>
