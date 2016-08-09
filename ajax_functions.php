ce93659efec23be0e47932a3fa50fd495deef39e
austingraham24 <austingraham24@gmail.com>
2016-08-09 13:02:42 -0500

    new function to adjust class average when assigments are graded

--- ajax_functions.php
+++ ajax_functions.php
@@ -1,5 +1,11 @@
 <?php
 //print("Page Loaded<br/>");
+
+$m = new MongoClient();
+$db = $m->selectDB("gamification_db");
+$user_quests = new MongoCollection( $db, "users-quests");
+$user_courses = new MongoCollection( $db, "users-courses");
+
 $response = "";
 
 if(isset($_GET['action'])){
@@ -20,9 +26,6 @@ if(isset($_GET['action'])){
 function changeGrade($user,$questId,$grade){
 	//print("DB Function <br/>");
 	//print($user."_".$questId."_".$grade."<br/>");
-	$m = new MongoClient();
-    $db = $m->selectDB("gamification_db");
-    $user_quests = new MongoCollection( $db, "users-quests");
     //print_r(array('user_id' => $user, 'quest_id' => $questId));
     //print("<br/>");
     $cursor = $user_quests->findOne(array('user_id' => $user, 'quest_id' => $questId));
@@ -31,8 +34,26 @@ function changeGrade($user,$questId,$grade){
      	$update=array('$set'=>array('grade' => $grade, 'status' => 'graded'));
 		$user_quests->update(array('user_id' => $user, 'quest_id' => $questId),$update);
 		//header("Location: profile.php");
+		updateUserExperience($user,$cursor["course_id"]);
     }
 }
+
+function updateUserExperience($user,$course){
+	$userQuests = $user_quests->find(array('user_id' => $user, 'course_id' => $course));
+	$userQuests->fields(array('grade' => true,'_id' => false));
+	$expTotal=0;
+	foreach ($userQuests as $doc) {
+        foreach ($doc as $k => $v) {
+        	$expTotal = $expTotal + $v;
+        }
+	}
+	$userCourse = $user_courses->findOne(array('user_id' => $user, 'course_id' => $course));
+	$update=array('$set'=>array('xp' => $expTotal));
+	$user_courses->update(array('user_id' => $user, 'course_id' => $course),$update);
+}
+
+
+
 //print('End');
 //return response;
 ?>
