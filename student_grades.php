a954297debe8d6c402ee728e1f830edd2fb40c3c
austingraham24 <austingraham24@gmail.com>
2016-08-09 11:53:04 -0500

    milestone isnt working

--- student_grades.php
+++ student_grades.php
@@ -41,7 +41,7 @@
 			  </div>
 			</div>
 			<div class="centered-text">
-				<p>EXP Required for Next Milestone (<?php print($letterMilestone);?>): <?php print($milestoneValue-$userXP);?></p>
+				<p>EXP Required for Next Milestone (<?php print($letterMilestone);?>): <?php //print($milestoneValue-$userXP);?></p>
 			</div>
 		</div>
 	</div>
