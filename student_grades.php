30137fc4ef2dd00c0dc4acf037a5827555219739
austingraham24 <austingraham24@gmail.com>
2016-08-09 10:11:08 -0500

    adding progress bar

--- student_grades.php
+++ student_grades.php
@@ -1,5 +1,12 @@
 <div class="col-xs-12">
 	<div class="box">
-		<h1>Test</h1>
+		<div>
+			<div class="progress">
+			  <div class="progress-bar" role="progressbar" aria-valuenow="70"
+			  aria-valuemin="0" aria-valuemax="100" style="width:70%">
+			    70%
+			  </div>
+			</div>
+		</div>
 	</div>
 </div>
