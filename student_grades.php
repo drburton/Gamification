24bfec124bccfdbf8b84d8c05c3e4a2880d38f9f
austingraham24 <austingraham24@gmail.com>
2016-08-09 10:13:19 -0500

    formatting bar

--- student_grades.php
+++ student_grades.php
@@ -2,12 +2,12 @@
 	<div class="box">
 		<div>
 			<h1>Test</h1>
-			<div class="progress">
+			<div class="progress" style="width:70%">
 			  <div class="progress-bar" role="progressbar" aria-valuenow="70"
 			  aria-valuemin="0" aria-valuemax="100" style="width:70%">
 			    70%
 			  </div>
-			</div>
+			</div><br/>
 		</div>
 	</div>
 </div>
