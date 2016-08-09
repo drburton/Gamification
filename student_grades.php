0f814274e22327badf421ae418f91f689924f07c
austingraham24 <austingraham24@gmail.com>
2016-08-09 10:37:59 -0500

    var for grade percentage

--- student_grades.php
+++ student_grades.php
@@ -2,14 +2,16 @@
     $user_results = array('course_id' => $course, 'user_id'=> $_SESSION["login"]);
     $userCourseCursor = $collection2->findOne($user_results);
     $userXP = $userCourseCursor["xp"];
+    $gradePercentage = $userXP/$maxEXP;
 ?>
 
 <div class="col-xs-12">
 	<div class="box">
 		<div>
+			<?php print($gradePercentage);?>
 			<h1 class="centered-text">Course Progress: <?php print($userXP);?>XP</h1>
 			<div class="progress horizontal-centered" style="width:80%;">
-			  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="90"
+			  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?print(80); ?>"
 			  aria-valuemin="0" aria-valuemax="100" style="width:70%">
 			    70%
 			  </div>
