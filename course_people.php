898c98cc5dc67d7e137688234959c59f5950191d
austingraham24 <austingraham24@gmail.com>
2016-09-13 10:18:19 -0500

    removing sort

--- course_people.php
+++ course_people.php
@@ -114,7 +114,7 @@
                                         $user_results = array('course_id' => $course);
                                         $userCourseCursor = $collection2->find($user_results);
                                         $userCourseCursor->fields(array('user_id' => true,'user_role' => true,'_id' => false));
-                                        $userCourseCursor = $userCourseCursor->sort(array("user_id"=>1)); //Sort by title
+                                        //$userCourseCursor = $userCourseCursor->sort(array("user_id"=>1)); //Sort by title
                                         $userArray = [];
 
                                         $userCollection = new MongoCollection( $db, "users");
