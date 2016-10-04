eaa703cc4d9fa451ba749d9de236e1de8e23003e
Austin Graham <austingraham24@gmail.com>
2016-10-04 10:25:39 -0500

    aha! Perhaps...

--- student_grades.php
+++ student_grades.php
@@ -57,7 +57,7 @@
                         </thead>
                         <tbody>
                         	<?php 
-                        		$userQuestResults = array('course_id' => $course, 'user_id' => $_SESSION["name"]);
+                        		$userQuestResults = array('course_id' => $course, 'user_id' => $_SESSION["login"]);
 						        $userQuestCursor = $userQuestsCollection->find($userQuestResults);
 						        $userQuestCursor->fields(array('title' => true,'grade' => true, "_id" => false));
 						        $userQuestCursor=$userQuestCursor->sort(array("title"=>1)); //Sort by title
@@ -76,7 +76,7 @@
 						            	print($k);
 						            	//$totalXP=0;
 						            }
-						            print("</td><td>test</td></tr>");
+						            print("</td><td>".$title."</td></tr>");
 					          	}
 
                         	?>
