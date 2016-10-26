0afa57a276e1eb783659d5328463e4970aa3d463
austingraham24 <austingraham24@gmail.com>
2016-10-25 20:55:01 -0500

    more recongifuring

--- dashboard.php
+++ dashboard.php
@@ -59,9 +59,9 @@
                                 </div>
                             </div>
                             <div class=\"box-body\">
-                            <h3 class=\"box-title\">Role: ".$role."</h3>
                             </div><!-- /.box-body -->
                             <div class=\"box-footer\">
+                                <h4>Role: ".$role."</h4>
                                 <div class=\"progress\">
                                   <div class=\"progress-bar\" role=\"progressbar\" aria-valuenow=\"60\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: 60%;\">
                                     60%
